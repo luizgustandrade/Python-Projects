d599e3a6b3e1923cda8ab9bd721675c2e6c1619a
luizgustandrade <153747930+luizgustandrade@users.noreply.github.com>
2025-11-13 08:57:30 -0500

    Update llama3wOpenAI.py

--- llama3wOpenAI.py
+++ llama3wOpenAI.py
@@ -2,12 +2,12 @@ from openai import OpenAI
 
 client = OpenAI(
   base_url = "https://integrate.api.nvidia.com/v1",
-  api_key = "xxxx"
+  api_key = "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"
 )
 
 completion = client.chat.completions.create(
   model="meta/llama3-8b-instruct",
-  messages=[{"role":"user","content":"Tell me tips about Los Angeles."}],
+  messages=[{"role":"user","content":"Tell me everything that you find about in internet this profile https://www.instagram.com/phcabral and can you also list youtube channels or other profiles that I can search and access ? "}],
   temperature=0.5,
   top_p=1,
   max_tokens=1024,
