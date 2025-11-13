00cad3453352ea8d8b4a839d7be79263fb85eeb7
luizgustandrade <153747930+luizgustandrade@users.noreply.github.com>
2025-11-13 08:57:39 -0500

    Update test4.py

--- test4.py
+++ test4.py
@@ -1,2 +1,6 @@
-import crewai as crew
-print(dir(crew))  
+import edge_tts
+
+
+ts = edge_tts.Communicate(text, voice=voice, rate=rate)
+await tts.save(out)
+await tts.session.close()   # ensures the websocket and HTTP session are torn down
